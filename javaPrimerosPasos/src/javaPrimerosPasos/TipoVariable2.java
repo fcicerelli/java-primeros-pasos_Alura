43763d758e4b3d743b4f698ee6751da188eb7f58
fcicerelli <fcicerellitic@gmail.com>
2025-02-25 21:50:50 -0300

    actualizacion gitignore

--- javaPrimerosPasos/src/javaPrimerosPasos/TipoVariable2.java
+++ javaPrimerosPasos/src/javaPrimerosPasos/TipoVariable2.java
@@ -3,7 +3,8 @@ package javaPrimerosPasos;
 public class TipoVariable2 {
 	public static void main(String args[]) {
 
-        double variable1 = 230.89;
-        int variable1Entero =
+    double variable1 = 230.89;
+    int variable1Entero = (int) variable1;
+    System.out.println(variable1Entero);
 	}
 }
